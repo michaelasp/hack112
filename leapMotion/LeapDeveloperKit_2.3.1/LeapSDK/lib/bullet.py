ece51030e075a6110dcd7f36b787bfa068c6bcd1
Unknown <yuandy333@gmail.com>
2018-11-09 23:20:05 -0500

    bullet class and spawner

---
+++ leapMotion/LeapDeveloperKit_2.3.1/LeapSDK/lib/bullet.py
@@ -0,0 +1,29 @@
+class Bullet(object):
+    # Model
+    def __init__(self, cx, cy, angle, speed):
+        # A bullet has a position, a size, a direction, and a speed
+        self.cx = cx
+        self.cy = cy
+        self.r = 5
+        self.angle = angle
+        self.speed = speed
+        
+    # View
+    def draw(self, canvas):
+        canvas.create_oval(self.cx - self.r, self.cy - self.r, 
+                           self.cx + self.r, self.cy + self.r,
+                           fill="white", outline=None)
+
+    # Controller
+    def moveBullet(self):
+        # Move according to the original trajectory
+        self.cx += math.cos(math.radians(self.angle))*self.speed
+        self.cy -= math.sin(math.radians(self.angle))*self.speed 
+        
+    def isOffscreen(self, width, height):
+        # Check if the bullet has moved fully offscreen
+        return (self.cx + self.r <= 0 or self.cx - self.r >= width) and \
+               (self.cy + self.r <= 0 or self.cy - self.r >= height)
+
+
+    
--- leapMotion/LeapDeveloperKit_2.3.1/LeapSDK/lib/defender.py
+++ leapMotion/LeapDeveloperKit_2.3.1/LeapSDK/lib/defender.py
@@ -1,25 +1,80 @@
 # https://developer.leapmotion.com/sdk/v2
 
 #import Leap, sys, thread, time 
-import os, sys, inspect, thread, time
-sys.path.insert(0, "C:\Users\Joshua Moavenzadeh\Desktop\LeapDeveloperKit_3.2.0+45899_win\LeapSDK\lib/x86")
+# import os, sys, inspect, thread, time
+# sys.path.insert(0, "C:\Users\Joshua Moavenzadeh\Desktop\LeapDeveloperKit_3.2.0+45899_win\LeapSDK\lib/x86")
 
-import Leap
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+# import Leap
+# from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
-
+import bullet
+import random
+import math
 from Tkinter import *
 
 ####################################
 # customize these functions
 ####################################
+class Bullet(object):
+    # Model
+    def __init__(self, cx, cy, angle, speed):
+        # A bullet has a position, a size, a direction, and a speed
+        self.cx = cx
+        self.cy = cy
+        self.r = 5
+        self.angle = angle
+        self.speed = speed
+        
+    # View
+    def draw(self, canvas):
+        canvas.create_oval(self.cx - self.r, self.cy - self.r, 
+                           self.cx + self.r, self.cy + self.r,
+                           fill="red", outline=None)
 
+    # Controller
+    def moveBullet(self):
+        # Move according to the original trajectory
+        self.cx += math.cos(math.radians(self.angle))*self.speed
+        self.cy -= math.sin(math.radians(self.angle))*self.speed 
+        
+    def isOffscreen(self, width, height):
+        # Check if the bullet has moved fully offscreen
+        return (self.cx + self.r <= 0 or self.cx - self.r >= width) and \
+               (self.cy + self.r <= 0 or self.cy - self.r >= height)
+
+    
+class BulletSpawner(object):
+    import random
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+    def makeBullet(self, screenWidth, screenHeight):
+       
+        side = random.choice(["topbot"]) #,"leftright"])
+        if side == "topbot":
+            x = random.randint(0,screenWidth)
+            y = 0 #random.choice([0,screenHeight])
+        elif side == "leftright":
+            x = random.choice([0,screenWidth])
+            y = random.randint(0,screenHeight)
+        
+        speed = random.randint(10,40)
+        
+        if y == 0:
+            angle = math.degrees(math.atan((y *1. - screenHeight/2)/(screenWidth/2 - x)))
+        else:
+            angle = 45
+        
+        print("(%d,%d)" % (x,y), "(300,300)", angle)
+        
+        return Bullet(x, y, angle, speed)
+        
 def init(data):
-    data.controller = Leap.Controller()
-    data.frame = data.controller.frame()
-    data.fingerNames = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-    data.boneNames = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
-
+    data.timer = 0
+    data.spawner = BulletSpawner(0,0)
+    data.bullets = []
+    
 def mousePressed(event, data):
     # use event.x and event.y
     pass
@@ -29,64 +84,22 @@ def keyPressed(event, data):
     pass
 
 def timerFired(data):
-    updateLeapMotionData(data)
-    printLeapMotionData(data)
+    data.timer += 1
+    if data.timer % 10 == 0:
+        data.bullets.append(data.spawner.makeBullet(data.width,data.height))
+        
+    for bullet in data.bullets:
+        bullet.moveBullet()
+        if bullet.isOffscreen(data.width, data.height):
+            data.bullets.remove(bullet)
 
-def updateLeapMotionData(data):
-    data.frame = data.controller.frame()
-
-def printLeapMotionData(data):
-    frame = data.frame
-
-    print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (
-          frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
-
-    # Get hands
-    for hand in frame.hands:
-
-        handType = "Left hand" if hand.is_left else "Right hand"
-
-        print "  %s, id %d, position: %s" % (
-            handType, hand.id, hand.palm_position)
-
-        # Get the hand's normal vector and direction
-        normal = hand.palm_normal
-        direction = hand.direction
-
-        # Calculate the hand's pitch, roll, and yaw angles
-        print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
-            direction.pitch * Leap.RAD_TO_DEG,
-            normal.roll * Leap.RAD_TO_DEG,
-            direction.yaw * Leap.RAD_TO_DEG)
-
-        # Get arm bone
-        arm = hand.arm
-        print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
-            arm.direction,
-            arm.wrist_position,
-            arm.elbow_position)
-
-        # Get fingers
-        for finger in hand.fingers:
-
-            print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
-                data.fingerNames[finger.type],
-                finger.id,
-                finger.length,
-                finger.width)
-
-            # Get bones
-            for b in range(0, 4):
-                bone = finger.bone(b)
-                print "      Bone: %s, start: %s, end: %s, direction: %s" % (
-                    data.boneNames[bone.type],
-                    bone.prev_joint,
-                    bone.next_joint,
-                    bone.direction)
 
 def redrawAll(canvas, data):
-    # draw in canvas
-    pass
+    x,y = data.width/2, data.height/2
+    canvas.create_oval(x - 10, y - 10, x+10,y+10)
+    for bullet in data.bullets:
+        bullet.draw(canvas)
+        
 
 ####################################
 # use the run function as-is
@@ -135,4 +148,4 @@ def run(width=300, height=300):
     root.mainloop()  # blocks until window is closed
     print("bye!")
 
-run(400, 200)
+run(600, 600)
